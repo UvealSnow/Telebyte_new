ca18159501406a1e08dc5897e774a9841cf5775a
UvealSnow <kevhah92@gmail.com>
2016-03-02 18:08:54 -0600

    Add navbar

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Telebyte SA de CV</title>
+		<link href="assets/css/telebyte.css" rel="stylesheet">
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/angular.min.js"></script>
+	</head>
+	<body>
+		<div class="full-width black-bar">
+			<div class="logo">
+				<a href="./"><img src="assets/img/logo_white.png" class="logo"></a>
+			</div>
+			<div class="serv">
+				<p>Servicio al Cliente</p>
+				<p>+52 (444) 198 - 29 - 29</p>
+			</div>
+			<div class="lang">
+				<a href="#">Lenguaje</a>
+			</div>
+			<div class="hamb hidden-dt">
+				
+			</div>
+		</div>
+		<div class="top-page full-width">
+			<div class="circuit-content">
+				<div class="navbar-container">
+					<div class="main-navbar">
+						
+					</div>
+				</div>
+				<div class="logo-container full-width">
+					<div class="logo">
+						<a href="#">
+							<img src="assets/img/logos/avaya.png" class="logo">
+						</a>
+					</div>
+					<div class="logo">
+						<a href="#">
+							<img src="assets/img/logos/cisco.png" class="logo">
+						</a>
+					</div>
+					<div class="logo">
+						<a href="#">
+							<img src="assets/img/logos/hirschmann.png" class="logo">
+						</a>
+					</div>
+					<div class="logo">
+						<a href="#">
+							<img src="assets/img/logos/bosch.png" class="logo">
+						</a>
+					</div>
+					<div class="logo">
+						<a href="#">
+							<img src="assets/img/logos/faragauss.png" class="logo">
+						</a>
+					</div>
+					<div class="logo">
+						<a href="#">
+							<img src="assets/img/logos/panduit.png" class="logo">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
